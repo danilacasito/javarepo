87ae4ff94b65a61067b551fd3a0a5309316825df
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2023-06-21 08:35:28 +0000

    Add javadocs

--- src/main/java/me/elordenador/javautils/OSUtils.java
+++ src/main/java/me/elordenador/javautils/OSUtils.java
@@ -1,7 +1,15 @@
 package me.elordenador.javautils;
 
-
+/**
+ * This class is used for some Operating System utilities
+ * @author elordenador
+ */
 public class OSUtils {
+    /**
+     * This static method can be used to identify the OS that the user is using.
+     * @version 0.1
+     * @return String
+     */
     public static String getOS() {
         String OS = System.getProperty("os.name", "generic").toLowerCase();
         
