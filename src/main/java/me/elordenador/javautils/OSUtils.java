76559272458908401f42e5584eb5c4d9e5fe224b
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2023-06-21 08:10:45 +0000

    Fix error ";" expected

--- src/main/java/me/elordenador/javautils/OSUtils.java
+++ src/main/java/me/elordenador/javautils/OSUtils.java
@@ -8,11 +8,11 @@ public class OSUtils {
         if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
             return "MacOS";
         } else if (OS.indexOf("win") >= 0) {
-            return "Windows"
+            return "Windows";
         } else if (OS.indexOf("nux") >= 0) {
-            return "Linux"
+            return "Linux";
         } else {
-            return "OtherOS"
+            return "OtherOS";
         }
         
     }
