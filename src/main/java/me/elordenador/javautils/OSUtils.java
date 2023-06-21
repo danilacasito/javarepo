f5c5d2ce933b5b48b19330477915e0e739a04fca
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2023-06-21 08:06:29 +0000

    Added OSUtils class and configured build.gradle

---
+++ src/main/java/me/elordenador/javautils/OSUtils.java
@@ -0,0 +1,19 @@
+package me.elordenador.javautils;
+
+
+public class OSUtils {
+    public static String getOS() {
+        String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+        
+        if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+            return "MacOS";
+        } else if (OS.indexOf("win") >= 0) {
+            return "Windows"
+        } else if (OS.indexOf("nux") >= 0) {
+            return "Linux"
+        } else {
+            return "OtherOS"
+        }
+        
+    }
+}
